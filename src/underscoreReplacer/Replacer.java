151acee1590c4264808accf35d51504f1445fcb4
Jelani akoto <44904921+Jelani-Ak@users.noreply.github.com>
2020-05-29 19:54:36 +0100

    More refactoring and small adjustment to timer

--- src/underscoreReplacer/Replacer.java
+++ src/underscoreReplacer/Replacer.java
@@ -6,7 +6,6 @@ import java.awt.Font;
 import java.awt.GridBagConstraints;
 import java.awt.GridBagLayout;
 import java.awt.GridLayout;
-import java.awt.Insets;
 import java.awt.Toolkit;
 import java.awt.datatransfer.Clipboard;
 import java.awt.datatransfer.StringSelection;
@@ -16,6 +15,7 @@ import java.awt.event.FocusEvent;
 import java.awt.event.FocusListener;
 import java.awt.event.KeyEvent;
 import java.awt.event.KeyListener;
+import javax.swing.BorderFactory;
 import javax.swing.BoxLayout;
 import javax.swing.JButton;
 import javax.swing.JFrame;
@@ -28,18 +28,18 @@ import javax.swing.event.DocumentEvent;
 import javax.swing.event.DocumentListener;
 
 class Replacer extends JFrame implements ActionListener, KeyListener, FocusListener {
-  
+
   private JLabel labelCopyState;
 
   private JTextField textfieldUserInput;
   private JTextField textfieldOutputText;
-  
+
   private boolean textfieldPlaceholderActive = true;
-  
+
   private final String placeholderText = "Enter Text";
 
   private final Font arial = new Font("Arial", Font.PLAIN, 14);
-  
+
   public static void main(String[] args) {
     Replacer replacer = new Replacer();
   }
@@ -49,26 +49,28 @@ class Replacer extends JFrame implements ActionListener, KeyListener, FocusListe
   }
 
 //<editor-fold defaultstate="collapsed" desc="Action Events">
-  @SuppressWarnings("SpellCheckingInspection")
   @Override
   public void actionPerformed(ActionEvent ae) {
-    if ("CopyText".equals(ae.getActionCommand())) { //If copy text button is pressed
-      Timer timer = new Timer(2000, (ActionEvent e) -> labelCopyState.setText(null));
-      timer.restart();
+    //<editor-fold defaultstate="collapsed" desc="Copy to Clipboard state">
+    if ("CopyText".equals(ae.getActionCommand())) {
+      Timer timer = new Timer(2000, event -> {
+        labelCopyState.setText(null);
+      });
 
       if ((textfieldOutputText.getText().equals("")) || (textfieldOutputText.getText() == null)) {
-        timer.start();
         labelCopyState.setText("Nothing copied");
+        timer.start();
 
       } else if ((!textfieldOutputText.getText().equals("Enter Text")) && (textfieldUserInput.getText().length() > 0)) {
-        timer.start();
         labelCopyState.setText("Copied to Clipboard");
+        timer.start();
 
-        StringSelection stringSelection = new StringSelection(textfieldOutputText.getText()); //Transfer content in outputText to stringSelection
-        Clipboard clipboard = Toolkit.getDefaultToolkit().getSystemClipboard(); //Initialize the variable clipboard and assign system clipboard to it
-        clipboard.setContents(stringSelection, null); //Set system clipboard content to text in stringSeleciton (outputText)
+        StringSelection stringSelection = new StringSelection(textfieldOutputText.getText());
+        Clipboard clipboard = Toolkit.getDefaultToolkit().getSystemClipboard();
+        clipboard.setContents(stringSelection, null);
       }
     }
+    //</editor-fold>
   }
 
   @Override
@@ -109,21 +111,22 @@ class Replacer extends JFrame implements ActionListener, KeyListener, FocusListe
     GridBagConstraints gbc = new GridBagConstraints();
     gbc.fill = GridBagConstraints.BOTH;
 
-    JPanel outPanel = new JPanel();
-    outPanel.setBorder(new EmptyBorder(15, 15, 15, 15));
-    outPanel.setLayout(new BoxLayout(outPanel, BoxLayout.X_AXIS));
+    JPanel mainPanel = new JPanel();
+    mainPanel.setBorder(new EmptyBorder(15, 15, 15, 15));
+    mainPanel.setLayout(new BoxLayout(mainPanel, BoxLayout.X_AXIS));
 
-    JPanel cenPanel = new JPanel();
-    cenPanel.setLayout(new BorderLayout());
+    JPanel centralPanel = new JPanel();
+    centralPanel.setLayout(new BorderLayout());
 
-    JPanel botPanel = new JPanel(new GridBagLayout());
+    JPanel panelUserInput = new JPanel(new GridBagLayout());
+    JPanel panelUserOutput = new JPanel(new GridBagLayout());
 
-    JPanel textContainer = new JPanel();
-    textContainer.setLayout(new GridLayout(1, 3));
+    JPanel labelContainer = new JPanel();
+    labelContainer.setLayout(new GridLayout(1, 3));
 
-    JPanel txtOne = new JPanel();
-    JPanel txtTwo = new JPanel();
-    JPanel txtThr = new JPanel();
+    JPanel panelCharacterCount = new JPanel();
+    JPanel panelCopyState = new JPanel();
+    JPanel panelUnderscoreCount = new JPanel();
 
     JPanel buttonContainer = new JPanel();
     buttonContainer.setLayout(new BoxLayout(buttonContainer, BoxLayout.X_AXIS));
@@ -142,29 +145,38 @@ class Replacer extends JFrame implements ActionListener, KeyListener, FocusListe
 
     labelCopyState = new JLabel();
 
-    textContainer.add(txtOne);
-    textContainer.add(txtTwo);
-    textContainer.add(txtThr);
+    labelContainer.add(panelCharacterCount);
+    labelContainer.add(panelCopyState);
+    labelContainer.add(panelUnderscoreCount);
 
-    txtOne.add(labelCharacters);
-    txtOne.add(labelCharacterCount);
-    txtTwo.add(labelCopyState);
-    txtThr.add(labelUnderscores);
-    txtThr.add(labelUnderscoreCount);
+    panelCharacterCount.add(labelCharacters);
+    panelCharacterCount.add(labelCharacterCount);
+    panelCopyState.add(labelCopyState);
+    panelUnderscoreCount.add(labelUnderscores);
+    panelUnderscoreCount.add(labelUnderscoreCount);
 
+    //<editor-fold defaultstate="collapsed" desc="Output Textfield">
     textfieldOutputText = new JTextField();
     textfieldOutputText.setFont(arial);
     textfieldOutputText.setEditable(false);
     gbc.weightx = 1.0;
     gbc.gridx = 0;
-    gbc.gridy = 0;
-    botPanel.add(textfieldOutputText, gbc);
+    gbc.gridy = 1;
+    panelUserOutput.add(textfieldOutputText, gbc);
+    //</editor-fold>
 
+    //<editor-fold defaultstate="collapsed" desc="User input Textfield">
     textfieldUserInput = new JTextField();
     textfieldUserInput.setFont(arial);
     textfieldUserInput.addFocusListener(this);
+    textfieldUserInput.setBorder(BorderFactory.createCompoundBorder(
+            textfieldUserInput.getBorder(),
+            BorderFactory.createEmptyBorder(3, 0, 3, 0)));
+    gbc.weightx = 1.0;
+    gbc.gridx = 0;
+    gbc.gridy = 0;
+    panelUserInput.add(textfieldUserInput, gbc);
     textfieldUserInput.getDocument().addDocumentListener(new DocumentListener() { //Set up the text field to check for changes
-
       @Override
       public void changedUpdate(DocumentEvent e) {
         update();
@@ -172,7 +184,7 @@ class Replacer extends JFrame implements ActionListener, KeyListener, FocusListe
 
       @Override
       public void removeUpdate(DocumentEvent e) {
-        update(); //Deletion 
+        update(); //Deletion
       }
 
       @Override
@@ -182,8 +194,8 @@ class Replacer extends JFrame implements ActionListener, KeyListener, FocusListe
 
       //Replace underscores with spaces
       private void update() {
-        long underscoreCount = textfieldUserInput.getText().chars().filter(ch -> ch == '_').count(); //Filter out underscores
-        labelUnderscoreCount.setText(String.valueOf(underscoreCount));
+        long underscoreCount = textfieldUserInput.getText().chars().filter(ch -> ch == '_').count(); //Filter underscores
+        labelUnderscoreCount.setText(String.valueOf(underscoreCount)); //Count total underscores
 
         if (!textfieldUserInput.getText().equals("Enter Text")) {
           labelCharacterCount.setText(textfieldUserInput.getText().length() + "");
@@ -196,26 +208,28 @@ class Replacer extends JFrame implements ActionListener, KeyListener, FocusListe
         }
       }
     });
+    //</editor-fold>
 
     JButton copyText = new JButton("Copy");
     gbc.weightx = 0.01;
     gbc.gridx = 1;
-    gbc.gridy = 0;
+    gbc.gridy = 1;
     copyText.setActionCommand("CopyText");
     copyText.addActionListener(this);
-    botPanel.add(copyText, gbc);
+    panelUserOutput.add(copyText, gbc);
 
-    add(outPanel);
-    outPanel.add(cenPanel);
+    add(mainPanel);
+    mainPanel.add(centralPanel);
 
-    cenPanel.add(buttonContainer); //Copy Button
-    cenPanel.add(textfieldUserInput, BorderLayout.CENTER); //Enter Textfield
-    cenPanel.add(textContainer, BorderLayout.NORTH); //'Characters' and 'Underscores' label
-    cenPanel.add(botPanel, BorderLayout.SOUTH); //Output textfield
+    centralPanel.add(buttonContainer);
+    centralPanel.add(labelContainer, BorderLayout.NORTH);
+    centralPanel.add(panelUserInput, BorderLayout.CENTER);
+    centralPanel.add(panelUserOutput, BorderLayout.SOUTH);
 
     //<editor-fold defaultstate="collapsed" desc="Default Settings">
     setTitle("Underscore Replacer");
-    setSize(550, 155);
+    setSize(650, 165);
+    setResizable(false);
     setLocationRelativeTo(null);
     setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
     setVisible(true);
